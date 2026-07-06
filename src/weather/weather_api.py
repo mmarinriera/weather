f311faf7ccd2243dbd4b58151b6affae93b1171a
Miquel Marin Riera <miquel.marin.riera@gmail.com>
2026-07-06 11:59:39 +0200

    fix(api): check response code and log error messages

--- src/weather/weather_api.py
+++ src/weather/weather_api.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import requests
 from pydantic import AliasChoices
@@ -82,8 +83,24 @@ class OpenWeatherForecast(BaseModel):
     city: LocationData
 
 
+def _check_api_status_code(res: requests.Response) -> None:
+    if res.status_code == 200:
+        return
+
+    data = res.json()
+
+    if res.status_code < 400:
+        logger.warning(f"API response code {res.status_code}: '{data['message']}'.")
+        return
+
+    if res.status_code >= 400:
+        logger.critical(f"API response code {res.status_code}: '{data['message']}'. Aborting.")
+        sys.exit(1)
+
+
 def _get_location_coordinates(city_name: str, country_code: str, api_key: str) -> tuple[float, float]:
     res = requests.get(url=URL_GEO.format(city_name=city_name, country_code=country_code, limit=1, api_key=api_key))
+    _check_api_status_code(res)
     data = res.json()
     lat = data[0]["lat"]
     lon = data[0]["lon"]
@@ -95,6 +112,7 @@ def _get_location_coordinates(city_name: str, country_code: str, api_key: str) -
 def query_current_weather(city: str, country: str, api_key: str) -> OpenWeatherCurrent:
     lat, lon = _get_location_coordinates(city, country, api_key=api_key)
     res = requests.get(url=URL_CURRENT.format(lat=lat, lon=lon, api_key=api_key))
+    _check_api_status_code(res)
     data: OpenWeatherCurrent = OpenWeatherCurrent.model_validate(res.json())
     return data
 
@@ -103,5 +121,7 @@ def query_weather_forecast(city: str, country: str, hours: int, api_key: str) ->
     cnt = hours // 3 + 1
     lat, lon = _get_location_coordinates(city, country, api_key=api_key)
     res = requests.get(url=URL_FORECAST.format(lat=lat, lon=lon, cnt=cnt, api_key=api_key))
+    _check_api_status_code(res)
+
     data: OpenWeatherForecast = OpenWeatherForecast.model_validate(res.json())
     return data

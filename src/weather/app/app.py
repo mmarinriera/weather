b29a74e6da9e0571d0e3e697cec1fef313aa02bf
Miquel Marin-Riera <miquel.marin.riera@gmail.com>
2026-07-14 15:12:58 +0200

    feat: web interface (#2)
    Deploys a static website using fastapi.

---
+++ src/weather/app/app.py
@@ -0,0 +1,52 @@
+import logging
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+from weather.weather_api import ForecastEntry
+from weather.weather_api import query_current_weather
+from weather.weather_api import query_weather_forecast
+
+logger = logging.getLogger(__name__)
+
+MAX_HOURS = 5 * 24
+ABS_PATH = Path(__file__)
+
+
+class Settings(BaseSettings):
+    OPENWEATHER_API_KEY: str
+    WEATHER_LOCATION_CITY: str = "Munich"
+    WEATHER_LOCATION_COUNTRY: str = "DE"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+settings = Settings()
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=ABS_PATH.with_name("static")), name="static")
+templates = Jinja2Templates(directory=ABS_PATH.with_name("templates"))
+
+
+@app.get("/", response_class=HTMLResponse)
+async def weather(
+    request: Request, city: str = settings.WEATHER_LOCATION_CITY, country: str = settings.WEATHER_LOCATION_COUNTRY
+) -> HTMLResponse:
+    current_weather = query_current_weather(city, country, api_key=settings.OPENWEATHER_API_KEY)
+    forecast = query_weather_forecast(city=city, country=country, hours=MAX_HOURS, api_key=settings.OPENWEATHER_API_KEY)
+
+    grouped: dict[str, list[ForecastEntry]] = {}
+    for time_point in forecast.forecast:
+        if not grouped.get(time_point.day):
+            grouped[time_point.day] = []
+        grouped[time_point.day].append(time_point)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"location": f"{city}, {country}", "current": current_weather, "forecast": grouped},
+    )
--- src/weather/weather_api.py
+++ src/weather/weather_api.py
@@ -8,6 +8,9 @@ from pydantic import AliasChoices
 from pydantic import BaseModel
 from pydantic import Field
 
+DATE_OUT_FMT = "%a %d %b"
+TIME_OUT_FMT = "%H:%Mh"
+
 URL_CURRENT = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
 URL_FORECAST = (
     "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&cnt={cnt}&appid={api_key}&units=metric"
@@ -37,6 +40,7 @@ class WeatherDescription(BaseModel):
     id: int
     main: str
     description: str
+    icon: str
 
 
 class CloudsData(BaseModel):
@@ -67,6 +71,14 @@ class ForecastEntry(BaseModel):
     snow: SnowData = SnowData()
     pop: float
 
+    @property
+    def day(self) -> str:
+        return datetime.fromtimestamp(self.dt).strftime(DATE_OUT_FMT)
+
+    @property
+    def time(self) -> str:
+        return datetime.fromtimestamp(self.dt).strftime(TIME_OUT_FMT)
+
 
 class OpenWeatherCurrent(BaseModel):
     dt: int
@@ -77,6 +89,47 @@ class OpenWeatherCurrent(BaseModel):
     rain: RainData = RainData()
     snow: SnowData = SnowData()
     timezone: int
+
+    @property
+    def day(self) -> str:
+        return datetime.fromtimestamp(self.dt).strftime(DATE_OUT_FMT)
+
+    @property
+    def time(self) -> str:
+        return datetime.fromtimestamp(self.dt).strftime(TIME_OUT_FMT)
+
+    @property
+    def theme(self) -> str:
+        match self.weather[0].icon:
+            case "01d":
+                return "sunny"
+
+            case "01n":
+                return "night"
+
+            case "02d" | "03d":
+                return "partly-cloudy"
+
+            case "02n" | "03n":
+                return "cloudy-night"
+
+            case "04d" | "04n":
+                return "cloudy"
+
+            case "09d" | "09n" | "10d" | "10n":
+                return "rain"
+
+            case "11d" | "11n":
+                return "storm"
+
+            case "13d" | "13n":
+                return "snow"
+
+            case "50d" | "50n":
+                return "fog"
+
+            case _:
+                return "default"
 
 
 class OpenWeatherForecast(BaseModel):
